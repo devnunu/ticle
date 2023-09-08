1a52a8f81783ba4291bdaac54a4f7c1ce6f33b9f
eunwoo <eunwoo@finda.co.kr>
2023-09-08 16:08:13 +0900

    네비게이션 모듈 생성

---
+++ navigation/api/src/main/java/com/devnunu/ticle/navigation/api/TicleNavigation.kt
@@ -0,0 +1,5 @@
+package com.devnunu.ticle.navigation.api
+
+interface TicleNavigation {
+
+}
---
+++ navigation/impl/src/main/java/com/devnunu/ticle/navigation/impl/TicleNavigationImpl.kt
@@ -0,0 +1,6 @@
+package com.devnunu.ticle.navigation.impl
+
+import com.devnunu.ticle.navigation.api.TicleNavigation
+
+class TicleNavigationImpl: TicleNavigation {
+}
